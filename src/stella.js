5761a26bbc92639d0cb88b2a4e8d69f48703f2ea
Scott Zeid <s@zeid.me>
2013-09-16 09:44:09 -0500

    jQuery selectors for combined CSS/JS files should NOT use the cachebuster

--- src/stella.js
+++ src/stella.js
@@ -180,10 +180,10 @@ function generateStaticTemplate($headCloned, $bodyCloned) {
  var $head = $headCloned.clone(); // in case someone passed an original
  var $body = $bodyCloned.clone(); // instead of a clone
  $.get("stella.combined.css?"+STELLA_CACHEBUSTER, function(data) {
-  $head.children("link[href='stella.combined.css?"+STELLA_CACHEBUSTER+"']")
+  $head.children("link[href^='stella.combined.css']")
   .replaceWith($("<style type='text/css'></style>").text("\n" + data + "  "));
   $.get("stella.combined.js?"+STELLA_CACHEBUSTER, function(data) {
-   $head.children("script[src='stella.combined.js?"+STELLA_CACHEBUSTER+"']")
+   $head.children("script[src^='stella.combined.js']")
    .replaceWith($("<script type='text/javascript'><"+"/script>").text("\n" + data + "  "));
    $head.children("script[src*='analytics']").remove();
    page += "\n <head>\n  " + trimString($head.html()) + "\n </head>";
